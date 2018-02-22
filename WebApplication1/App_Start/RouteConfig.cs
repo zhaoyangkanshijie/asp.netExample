c618314a5eb23b571bd932135c5cdd3f9fda9a74
runoob <test@runoob.com>
2018-02-22 15:22:35 +0800

    route test

---
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            #region 首页
+            routes.MapRoute(
+                "Home1", // 首页
+                "", // URL
+                new { controller = "home", action = "index" }
+            );
+            routes.MapRoute(
+                "Home2", // 首页2
+                "index.html", // index.html
+                new { controller = "home", action = "index" }
+            );
+            #endregion
+
+            routes.MapRoute(
+                "about", // 首页
+                "about", // URL
+                new { controller = "home", action = "about" }
+            );
+
+            routes.MapRoute(
+                "contact", // 首页
+                "contact", // URL
+                new { controller = "home", action = "contact" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();//必须
+            GlobalConfiguration.Configure(WebApiConfig.Register);//web api
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);//过滤器（可不修改）
+            RouteConfig.RegisterRoutes(RouteTable.Routes);//注册路由
+            BundleConfig.RegisterBundles(BundleTable.Bundles);//web form
+        }
+    }
+}
