6a26b48f320c680082d8a740f756b7957e657fd8
runoob <test@runoob.com>
2018-02-23 09:53:05 +0800

    ActionFilterAttribute

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Controllers.extends;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         [ActionName("index")]
+        [WebApplication1Adaptive]
         public ActionResult Index()
         {
             return View();
---
+++ WebApplication1/Controllers/extends/WebApplication1AdaptiveAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers.extends
+{
+    public class WebApplication1AdaptiveAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                string rawUrl = System.Web.HttpContext.Current.Request.RawUrl;
+                string userAgent = System.Web.HttpContext.Current.Request.UserAgent.ToLower();
+                
+                if (ifMobile(userAgent) && rawUrl.IndexOf("m")!=1)
+                {
+                    filterContext.Result = new RedirectResult(urlToMobile(rawUrl));
+                }
+                else if (!ifMobile(userAgent) && rawUrl.IndexOf("m") == 1)
+                {
+                    filterContext.Result = new RedirectResult(urlToPC(rawUrl));
+                }
+                return;
+            }
+            catch
+            {
+                //如果报错，返回首页
+                filterContext.Result = new RedirectResult("/");
+                return;
+            }
+        }
+
+        private bool ifMobile(string userAgent)
+        {
+            if (userAgent.Contains("iphone") || userAgent.Contains("android") || userAgent.Contains("ipad") || userAgent.Contains("ucweb") || userAgent.Contains("blackberry") || userAgent.Contains("symbianos") || userAgent.Contains("windows phone") || userAgent.Contains("opera mini"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string urlToMobile(string rawUrl)
+        {
+            string url = "/m" + rawUrl;
+            
+            return url;
+        }
+
+        private string urlToPC(string rawUrl)
+        {
+            string url = rawUrl.Substring(2);
+            
+            return url;
+        }
+
+        
+    }
+}
